3a5df0a2149ee9d95b582c29f783d77b45a168c9
Estebanan03000 <estebanidarragaospina29@gmail.com>
2026-03-06 07:31:36 -0500

    Cambio Esteban

--- Negocio/Prueba.py
+++ Negocio/Prueba.py
@@ -1,13 +1,13 @@
 def fibo(n):
-    if n <= 1:
+    if n == 1 or n == 0:
         return 1
     else:
         return fibo(n-1) + fibo(n-2)
     
 def imprimirHola():
-    print("Hola Mundo")
+    print("Hola Mundo, como estas?")
 
 
 def imprimirHastaN(n):
-    for i in range(n):
-        print(i)
+    for i in range(0, n):
+        print(f"numero: {i}")
