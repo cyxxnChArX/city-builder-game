8e574a424c1657e5c6296518aca49e9d59faf583
jaco1127 <jacobocastano1127@gmail.com>
2026-03-06 07:41:22 -0500

    Segundo cambio Jacobo

--- Negocio/Prueba.py
+++ Negocio/Prueba.py
@@ -2,9 +2,9 @@ def fibo(n):
     if n == 1 or n == 0:
         return 1
     else:
-        return fibo(n-1) + fibo(n-2)
+        return fibo(n-1) + fibo(n-2) + fibo(n-3)
     
-def imprimirHola():
+def salutation():
     print("Hola Mundo, como estas? La vida es buena")
     print("Hola Nico y Jaco, como estas?")
 
