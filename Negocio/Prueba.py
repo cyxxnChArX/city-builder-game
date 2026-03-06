96445d7a3caa56acdb2c60d7604994b9163202ba
cyxxnChAr <ninafaro3418@gmail.comgit config --global user.email ninafaro3418@gmail.com>
2026-03-06 07:44:41 -0500

    cambio de nicolasz en fibonacci

--- Negocio/Prueba.py
+++ Negocio/Prueba.py
@@ -2,7 +2,7 @@ def fibo(n):
     if n == 1 or n == 0:
         return 1
     else:
-        return fibo(n-1) + fibo(n-2)
+        return fibo(n-1) + fibo(n) #quite el n-2 
     
 def imprimirHola():
     print("cambio para dañar la linea 8")
